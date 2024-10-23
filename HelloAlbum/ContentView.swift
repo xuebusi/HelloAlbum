667aed2039b6990df2c54ff3b29db07e2f93305a
xuebusi <xuebusi@qq.com>
2024-10-23 23:32:23 +0800

    首页展示照片数量

--- HelloAlbum/ContentView.swift
+++ HelloAlbum/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var vm: PhotoViewModel
+    
     var body: some View {
         NavigationStack {
             NavigationLink {
                 PhotoGridView()
             } label: {
-                Text("所有照片")
+                Text("所有照片(\(vm.photos.count))")
             }
         }
     }
